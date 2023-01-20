9cdd18b0e52acaddd43df91626cdbc601fd8cbdc
Timothée Auffret <98650963+timpcmr@users.noreply.github.com>
2023-01-20 16:48:51 +0100

    Update Commande.java

--- src/main/java/org/example/modele/Commande.java
+++ src/main/java/org/example/modele/Commande.java
@@ -29,7 +29,7 @@ public class Commande implements Serializable {
     private Magasin magasin;
 
     //Une commande a plusieurs materiels
-    @ManyToMany
+    @ManyToMany(cascade = CascadeType.MERGE)
     private List<Materiel> materiels;
 
     //Chaque matériel a une quantité
